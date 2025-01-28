387c82e9af26f7a662c261f15187cfd551afc7fd
落雁沙 <fengxiaotx@163.com>
2025-01-28 12:12:38 +0800

    feat: tmp

--- src/builder/bundless/index.ts
+++ src/builder/bundless/index.ts
@@ -1,9 +1,7 @@
 import glob from "fast-glob"
-import swcTransformer from "./swc";
 import path from "node:path";
 import fs from "fs";
 import {getDeclarations} from "./dts";
-import ts from "typescript";
 import babelTransformer from "./babel";
 
 export type IBundlessFun = (ctx: TransformContext, content: string) => Promise<{ code?: string }>;
@@ -30,22 +28,21 @@ export interface TransformContext {
 }
 
 export const bundless = async (buildContext: BundlessContext) => {
-
     const files = glob.globSync("**/*", {cwd: "src"})
     console.log(files)
-    transformFiles(files, {cwd: buildContext.cwd})
+    transformFiles(files, {cwd: buildContext.cwd, output: buildContext.vctxConfig?.output || 'dist'})
 
 }
 
 const transformFiles = (files: string[], ops: {
     cwd?: string,
+    output: string
 }) => {
     for (const file of files) {
         const filePath = path.join(ops.cwd!, "src", file)
         const sourceContent = fs.readFileSync(filePath, "utf-8");
-        if ([".ts", ".tsx"].includes(path.extname(file))) {
-            console.log(path.dirname(file ));
-            const distFilePath = path.join(ops.cwd!, "dist", path.dirname(file),path.basename(file, path.extname(file)) + ".js")
+        if ([".vue",".js", ".jsx", ".ts", ".tsx"].includes(path.extname(file))) {
+            const distFilePath = path.join(ops.cwd!, ops.output, path.dirname(file), path.basename(file, path.extname(file)) + ".js")
             const parentPath = path.dirname(distFilePath);
             if (!fs.existsSync(parentPath)) {
                 fs.mkdirSync(parentPath, {recursive: true});
@@ -55,7 +52,6 @@ const transformFiles = (files: string[], ops: {
                 filePath: filePath,
                 distFilePath: distFilePath,
             }
-            console.log(ctx)
             babelTransformer(ctx, sourceContent).then(({code}) => {
                 fs.writeFileSync(distFilePath, code!)
             })
@@ -64,11 +60,11 @@ const transformFiles = (files: string[], ops: {
     const tsFiles = files.filter(file => {
         return [".ts", ".tsx"].includes(path.extname(file))
     }).map(file => {
-        return path.join(ops.cwd!,"src", file);
+        return path.join(ops.cwd!, "src", file);
     });
-    getDeclarations(tsFiles).then((dtsFiles)=>{
+    getDeclarations(tsFiles).then((dtsFiles) => {
         dtsFiles.forEach((dtsFile) => {
-            fs.writeFileSync(path.join(ops.cwd!,dtsFile.fileName),dtsFile.content)
+            fs.writeFileSync(path.join(ops.cwd!, dtsFile.fileName), dtsFile.content)
         })
     })
 }
